f44689ca973bfaec40f1b9a43c736ccf275159da
RUDYGAMES <rudygamesv2@gmail.com>
2023-04-04 09:04:07 +0200

    added eye show, hide password function

--- scrtipt.js
+++ scrtipt.js
@@ -7,10 +7,26 @@ const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 const allInput = document.querySelectorAll('.form-box input')
 
+const eyeBtns = document.querySelectorAll('.form-box .fa-regular')
+
+const showHidePassword = e => {
+	e.preventDefault()
+	if (e.target.matches('.fa-eye')) {
+		e.target.classList.remove('fa-eye')
+		e.target.classList.add('fa-eye-slash')
+		e.target.previousElementSibling.setAttribute('type', 'text')
+	} else {
+		e.target.classList.remove('fa-eye-slash')
+		e.target.classList.add('fa-eye')
+		e.target.previousElementSibling.setAttribute('type', 'password')
+	}
+}
+
+
 const showError = (input, msg) => {
 	const formBox = input.parentElement
 	const errorMsg = formBox.querySelector('.error-text')
-
+	
 	formBox.classList.add('error')
 	errorMsg.textContent = msg
 }
@@ -45,56 +61,49 @@ const checkPassword = (pass1, pass2) => {
 
 const checkMail = email => {
 	const re =
-		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-    if(re.test(email.value)){
-        clearError(email)
-    }
-    else{
-        showError(email, 'E-mail jest nieprawidłowy')
-    }
+	/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'E-mail jest nieprawidłowy')
+	}
 }
 
 const checkErrors = () => {
-    const allFormBox = document.querySelectorAll('.form-box')
-    let errorCount = 0
-
-    allFormBox.forEach(el => {
-        if(el.classList.contains('error')){
-            errorCount++
-        }
-    })
-
-    if(errorCount === 0){
-        popup.classList.add('show-popup')
-    }
-    // else{
-    //     popup.classList.remove('show-popup')
-    // }
+	const allFormBox = document.querySelectorAll('.form-box')
+	let errorCount = 0
+	
+	allFormBox.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+	
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
 }
-
-
-
+eyeBtns.forEach(el => {
+	el.addEventListener('click', showHidePassword)
+})
 
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
-
+	
 	checkForm(allInput)
 	checkLenght(username, 4)
 	checkLenght(pass, 8)
 	checkPassword(pass, pass2)
-    checkMail(email)
-    checkErrors()
+	checkMail(email)
+	checkErrors()
 })
 
 clearBtn.addEventListener('click', e => {
 	e.preventDefault()
 
-	// [username, pass, pass2, email].forEach(el => {
-	// 	el.value = ''
-	// })
 	allInput.forEach(el => {
 		el.value = ''
-        clearError(el)
+		clearError(el)
 	})
 })
